6eda206bf11e9de384ce23d490aa18cbcea61045
jps <jps@MacBook-Pro.fritz.box>
2020-03-26 12:21:44 +0100

    multiple changes

--- simple_forward_server/server.js
+++ simple_forward_server/server.js
@@ -2,114 +2,131 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 var cors = require("cors"); // Cors
 app.use(cors());
 var port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => res.send('Hello World!'))
+
+// *************** GETDATA ***************************************
 app.get('/getData', function (req, res) {
-  var request = require("request");
-  var reqURL = "https://hackathon-jps.eu-de.mybluemix.net/getData";
+    var request = require("request");
 
-  console.log("URL: \n", reqURL);
+    var httpHeaderOptions = {
+        accept: "application/json",
+        "content-type": "application/json",
+        apikey: req.headers.apikey
+    };
 
-  var httpHeaderOptions = {
-    accept: "application/json",
-    "content-type": "application/json",
-  };
+    var restoptions = {
+        method: "GET",
+        url: req.headers.route,
+        headers: httpHeaderOptions
+    };
 
-  var restoptions = {
-    method: "GET",
-    url: reqURL,
-    headers: httpHeaderOptions,
-  };
+    //    console.log("headers: " + JSON.stringify(req.headers));
+    //    console.log("GET DOCS: \n", JSON.stringify(restoptions));
 
-  console.log("send request");
-  request(restoptions, function (error, response, body) {
-    console.log("in request \n");
-    if (error) {
-      console.error("Failed: %s", error.message);
-      body = { "error": error.message };
-      res.status(400).json(body);
-    } else {
-      console.log("Success: \n", body);
-      res.status(200).json(body);
-    }
-  });
+    request(restoptions, function (error, response, body) {
+        console.log(typeof (body));
+        body_json = JSON.parse(body);
+        if (error) {
+            console.error("Failed: %s", error.message);
+            body = {
+                "error": error.message
+            };
+            res.status(400).json(body);
+        } else {
+            console.log("Success: \n", body);
+            res.status(200).json(body_json);
+        }
+    });
 });
 
+// *************** POST  DOC ***************************************
 app.post('/postData', function (req, res) {
-  var request = require("request");
-  var reqURL = "https://YOUR_NODE_RED_URL/postData";
+    var request = require("request");
 
-  console.log("URL: \n", reqURL);
-  console.log("POST Body: \n", JSON.stringify(req.body));
+    var httpHeaderOptions = {
+        accept: "application/json",
+        "content-type": "application/json",
+        apikey: req.headers.apikey
+    };
 
-  var httpHeaderOptions = {
-    accept: "application/json",
-    "content-type": "application/json",
-  };
+    var restoptions = {
+        method: "POST",
+        url: req.headers.route,
+        headers: httpHeaderOptions,
+        body: req.body,
+        json: true
+    };
 
-  var restoptions = {
-    method: "POST",
-    url: reqURL,
-    headers: httpHeaderOptions,
-    body: req.body,
-    json: true
-  };
+    console.log("headers: " + JSON.stringify(req.headers));
+    console.log("POST DOC: \n", JSON.stringify(restoptions));
+    request(restoptions, function (error, response, body) {
+        if (typeof (body) == 'object' && Object.keys(body).length === 0) {
+            // unknown error, empty resposne
+            res.status(400).json(body);
+        } else {
+            console.log("body: " + JSON.stringify(body));
+            if (error) {
+                console.error("Failed: %s", error.message);
+                body = {
+                    "error": error.message
+                };
+                res.status(400).json(body);
+            } else {
+                console.log("Success: \n", JSON.stringify(body));
+                res.status(200).json(body);
+            }
 
-  console.log("send request \n");
-
-  request(restoptions, function (error, response, body) {
-    console.log("in request \n");
-    if (error) {
-      console.error("Failed: %s", error.message);
-      body = { "error": error.message };
-      res.status(400).json(body);
-    } else {
-      console.log("Success: \n", JSON.parse(body));
-      res.status(200).json(body);
-    }
-  });
+        }
+    });
 });
 
+// *********************
 app.post('/watsonAssistant', function (req, res) {
-  var request = require("request");
-  var reqURL = "https://hackathon-jps.eu-de.mybluemix.net/watsonAssistant";
+    var request = require("request");
+    var reqURL = "https://hackathon-jps.eu-de.mybluemix.net/watsonAssistant";
 
-  console.log("URL: \n", reqURL);
-  console.log("POST Body: \n", JSON.stringify(req.body));
+    console.log("URL: \n", reqURL);
+    console.log("POST Body: \n", JSON.stringify(req.body));
 
-  var httpHeaderOptions = {
-    accept: "application/json",
-    "content-type": "application/json",
-  };
+    var httpHeaderOptions = {
+        accept: "application/json",
+        "content-type": "application/json",
+    };
 
-  var restoptions = {
-    method: "POST",
-    url: reqURL,
-    headers: httpHeaderOptions,
-    body: req.body,
-    json: true
-  };
+    var restoptions = {
+        method: "POST",
+        url: reqURL,
+        headers: httpHeaderOptions,
+        body: req.body,
+        json: true
+    };
 
-  console.log("send request \n");
+    console.log("send request \n");
 
-  request(restoptions, function (error, response, body) {
-    console.log("in request \n");
-    if (error) {
-      console.error("Failed: %s", error.message);
-      body = { "error": error.message };
-      res.status(400).json(body);
-    } else {
-      console.log("Success: \n", body[0]);
-      res.status(200).json(body[0]);
-    }
-  });
+    request(restoptions, function (error, response, body) {
+        console.log("in request \n");
+        if (error) {
+            console.error("Failed: %s", error.message);
+            body = {
+                "error": error.message
+            };
+            res.status(400).json(body);
+        } else {
+            console.log("Success: \n", body[0]);
+            res.status(200).json(body[0]);
+        }
+    });
 });
 
 
 // Start the server
 app.listen(port, function () {
-  console.log('simple forward server is running')
+    console.log('simple forward server is running')
 });
